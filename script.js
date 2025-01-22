3370aef8a0501c829af015dce670d8c0201a4b81
Xylo87 <theo.arbogast@hotmail.fr>
2025-01-23 00:35:12 +0100

    feat : ajout d'une fonction de randomization d'affichage des Pokémons + style général

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
-pokedex = document.getElementById("pokedex")
+
+const pokedex = document.getElementById("pokedex")
+
+const pokeButton = document.createElement("button")
+pokeButton.innerText = "Get random Pokemon !"
+pokeButton.classList.add("btn", "btn-success")
 
 async function call() {
-    
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/3")
-    const data = await response.json()
-    console.log(data)
 
+    let randomNumber = Math.floor(Math.random() * 1025) + 1
+    
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`)
+    let data = await response.json()
+    console.log(data)
+    
     let pokeName = data.name
     let pokeFaceURL = data.sprites.front_default
     
-    pokemon = document.createElement("div")
-    pokeCanva = document.createElement("img")
+    const pokemon = document.createElement("div")
+    const pokeTitle = document.createElement("h1")
+    const pokeCanva = document.createElement("img")
+    
+    pokeTitle.innerText = pokeName
     pokeCanva.setAttribute("src", pokeFaceURL)
     
-    pokemon.append(pokeName, pokeCanva)
+    pokemon.append(pokeTitle, pokeCanva, pokeButton)
     pokedex.append(pokemon)
-
+    
     pokemon.classList.add("pokemon")
 }
 
+
 call()
+
+
+pokeButton.addEventListener('click', function () {
+    document.querySelector(".pokemon").remove()
+    call()
+})
